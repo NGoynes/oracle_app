6ee37eaf70e175f816805f3ce079f543d388f4bc
ngoynes <nate.goynes@gmail.com>
2021-01-11 17:54:18 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaakysto57p6jdhpgv4ikly2qltdmd2af6lm7tr2mzxtgcawd37cboa"
   user_ocid = "ocid1.user.oc1..aaaaaaaafyhsucl7sf3eu4b737vuwyq6jnk44tfytennzol4rndrknrkpybq" 
-  private_key_path = "$HOME/.oci/tutorial.pem"
+  private_key_path = "home/oci/tutorial.pem"
   fingerprint = "a5:16:9d:b7:bd:c8:c3:92:24:21:87:a6:26:dc:2a:8f"
   region = "us-phoenix-1"
 }
